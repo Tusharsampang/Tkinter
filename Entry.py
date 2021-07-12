259e7811c86094f60adbabb46bf859d2b8b1865b
ACER <tusharsampang@gmail.com>
2021-07-12 11:11:34 +0545

    commit

--- Entry.py
+++ Entry.py
@@ -7,7 +7,7 @@ e1.pack()
 
 
 def myClick():
-    textoutput = "Hello" + e1.get()
+    textoutput = "Hello " + e1.get()
     myLabel = Label(root, text=textoutput)
     myLabel.pack()
     myButton1 = Button(root, text="Click")
